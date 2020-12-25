09061604a275ffefc3dd6af10f788df2e55728da
abhigyan <abhigyan709@gmail.com>
2020-12-25 12:51:22 +0530

    "Django Authentication Views Initiated"

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -15,8 +15,7 @@ def user_login(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Authenticated' \
-                                        'successfully')
+                    return HttpResponse('Authenticated Successfully')
                 else:
                     return HttpResponse('Disabled Account')
             else:
