d094995903a548ac5369b0da5a541eebc02f68dc
abhigyan <abhigyan709@gmail.com>
2020-12-25 17:22:21 +0530

    "Second official commit"

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     # previous logn view
     # path('login/', views.user_login, name='login'),
+    path('', views.dashboard, name='dashboard'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#new code implementations
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
