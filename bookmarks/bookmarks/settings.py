b7864ead44812ff2d75d49c47fa73901850489dd
abhigyan <abhigyan709@gmail.com>
2020-12-30 23:44:58 +0530

    "Messages View added successfully"

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -118,3 +118,8 @@ LOGOUT_URL = 'logout'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
